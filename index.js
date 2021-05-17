3fabec778e857c8e5f129e253e034ea05ca4980d
Hazar Saad ElDine <saadeldinehazar@gmail.com>
2021-05-17 14:51:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,43 +1,5 @@
-let message = 'Unauthorized Access';
-let rando
-let rando = Math.ceil( Math.random() * 1000 );
-
-function submitData(userName,userEmail){
-const formData = {
-  name: userName,
-  email: userEmail
-};
-
-const configObj = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify(formData)
-};
-
-return fetch("http://localhost:3000/users", configObj)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(json) {
-
-        body.innerHTML = json.id;
-            console.log(json);
-        })
-        .catch(function(error) {
-                 body.innerHTML = error.message;
-                 console.log(error.message);
-             });
-
-    if(formData)
-    {json.id=rando;
-body.innerText=json.id;
-    json+=formData;}
-  //  for(let i=0;i<json.length;i++)
-  //  {body.innerText=json.id;
-
-
-}
-submitData();
+function submitData( name, email ) { 
+   return fetch( 'http://localhost:3000/users', {   
+        method: "POST",      headers: {        "Content-Type": "application/json",        "Accept": "application/json"      }, 
+             body: JSON.stringify( {        name,        email      } )    } )    .then( function ( response ) {      return response.json()    } )    .then( function ( object ) {      document.body.innerHTML = object[ "id" ]    } )  
+  .catch( function ( error ) {      document.body.innerHTML = error.message    } )}
